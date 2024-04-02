6398249ee1de955b5196e63c3fdfb914c88f628e
LARCHIK <ovchinnikov20035696@gmail.com>
2024-04-02 11:04:27 +0500

    Add scrapper-frame

--- scrapper/src/main/java/edu/java/scrapper/configuration/ApplicationConfig.java
+++ scrapper/src/main/java/edu/java/scrapper/configuration/ApplicationConfig.java
@@ -3,6 +3,7 @@ package edu.java.scrapper.configuration;
 import jakarta.validation.constraints.NotNull;
 import java.time.Duration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.validation.annotation.Validated;
 
 @Validated
@@ -11,6 +12,12 @@ public record ApplicationConfig(
     @NotNull
     Scheduler scheduler
 ) {
+
+    @Bean
+    public Duration interval() {
+        return scheduler.interval;
+    }
+
     public record Scheduler(boolean enable, @NotNull Duration interval, @NotNull Duration forceCheckDelay) {
     }
 }
---
+++ scrapper/src/main/java/edu/java/scrapper/core/sheduled/LinkUpdaterScheduler.java
@@ -0,0 +1,38 @@
+package edu.java.scrapper.core.sheduled;
+
+import edu.java.scrapper.core.client.GithubClient;
+import edu.java.scrapper.core.client.StackOverflowClient;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@Log4j2
+@RequiredArgsConstructor
+public class LinkUpdaterScheduler {
+
+    private final GithubClient githubClient;
+    private final StackOverflowClient stackOverflowClient;
+
+    @Scheduled(fixedDelayString = "#{@interval}")
+    @SuppressWarnings("MagicNumber")
+    public void update() {
+
+        log.info("getting all tracked links ...");
+
+        String repo = "Tinkoff-Backend";
+        String owner = "larchik";
+        long questionId = 66675088;
+
+        log.info("getting all updates ...");
+
+        var newCommits = githubClient.getUpdates(repo, owner);
+        var newAnswers = stackOverflowClient.getUpdates(questionId);
+
+        log.info("saving all updates ...");
+
+        log.info("Done");
+    }
+
+}
